778ad872735cdc255ae448b89ff66b74ed8dc2cc
LorenzoBellone <lorenzo.bellone@outlook.it>
2024-10-01 20:03:25 +0400

    make faster

--- run_make.sh
+++ run_make.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v $PWD:/module/ --device /dev/ttyUSB0 --privileged -P nina_esp_mdf /bin/bash -c "cd /module; source /esp-mdf/export.sh; ls /;make clean ;make menuconfig; make all"
+docker run --rm -it -v $PWD:/module/ --device /dev/ttyUSB0 --privileged -P nina_esp_mdf /bin/bash -c "cd /module; source /esp-mdf/export.sh; ls /;make menuconfig; make -j 16"
